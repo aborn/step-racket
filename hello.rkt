c9802009f8f80897bf3062f964790e267efdbf5d
guobao.jiang <loveaborn@foxmail.com>
2015-08-17 23:02:54 +0800

    if statement usage

--- hello.rkt
+++ hello.rkt
@@ -6,7 +6,14 @@
   (substring str 4 7))
 
 (extract "the cat out of the bag")
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; if 语句
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(if (file-exists? "~/dpcode/eta/indextabicon/category.txt")
+    "category.txt exists!"
+    "cannot find exits!")
 
-(file-exists? "~/dpcode/eta/indextabicon/category.txt")
+(file-exists? "/Users/aborn/dpcode/eta/indextabicon/category.txt")
+
 (directory-exists? "/Users/aborn/dpcode")
 
