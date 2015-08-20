77e9094f2f03a8b6c0f204ac9a19eaefb1205ce5
guobao.jiang <loveaborn@foxmail.com>
2015-08-21 00:02:03 +0800

    string diff

--- hello.rkt
+++ hello.rkt
@@ -13,15 +13,26 @@
     "category.txt exists!"
     "cannot find exits!")
 
+;; 判断文件是否存在
 (file-exists? "/Users/aborn/dpcode/eta/indextabicon/category.txt")
 
+;; 判断文件夹是否存在
 (directory-exists? "/Users/aborn/dpcode")
 
-(define (diff strA strB)
-  (let ([sizeA (string-length strA)]
-        [sizeB (string-length strB)])
-    (for ([i (if (> sizeA sizeB) strB strA)])
-      (display i))))
+;; 求最小值
+(define (min valueA valueB)
+  (if (< valueA valueB)
+      valueA
+      valueB))
 
-(diff "fdaf" "fd")
+;; 提取出两个字符串中不相同的部分
+(define (diff strA strB)
+  (define lenA (string-length strA))
+  (define lenB (string-length strB))
+  (define minValue (min lenA lenB))
+  (if (< lenA lenB)
+      (display (substring strB minValue))
+      (display (substring strA minValue))))
+
+(diff "fd a中f 这是一个测试的字符串" "fd a中")
 
